eed38678008d4d5af90b7df76a3815447f38e65d
Jose F <bazoocaze@gmail.com>
2018-08-31 14:52:41 -0300

    Add joke text to 'joke' property of Model. Closes #5

--- src/main/java/br/com/jasf/sfgjokesapp/controllers/JokesController.java
+++ src/main/java/br/com/jasf/sfgjokesapp/controllers/JokesController.java
@@ -4,12 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.jasf.sfgjokesapp.services.JokesService;
+
 @Controller
 public class JokesController {
 
-	@RequestMapping(value = { "/", ""})
-	public String getRandomQuote(Model model)
-	{		
+	private JokesService jokesService;
+
+	public JokesController(JokesService jokesService) {
+		super();
+		this.jokesService = jokesService;
+	}
+
+	@RequestMapping(value = { "/", "" })
+	public String getRandomQuote(Model model) {
+		model.addAttribute("joke", jokesService.getRandomQuote());
 		return "chucknorris";
 	}
+
 }
